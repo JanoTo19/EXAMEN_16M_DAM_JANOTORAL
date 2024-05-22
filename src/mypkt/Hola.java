3093b2870b36e00e3fe84ed4bbd24b915ca9dde5
Jano Toral <jtoralsm@ceinmark.net>
2024-05-22 12:02:28 +0200

    prueba examen

---
+++ src/mypkt/Hola.java
@@ -0,0 +1,21 @@
+package mypkt;
+
+import java.util.Scanner;
+
+public class Hola {
+	
+	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner(System.in);
+		int num = 0;
+		
+		System.out.println("Buenos días Ceinmark, soy Jano");
+		
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+		num = Integer.parseInt(ent.nextLine());
+		
+
+		
+	}
+
+}
